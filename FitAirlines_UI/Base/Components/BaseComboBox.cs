d0a060aa50b597d5d05a27e12d8a2ad52a294bf0
Josip Rezić <josip.rezic@hotmail.com>
2020-02-02 10:45:32 +0100

    Introduce BaseComboBox. update existing forms

---
+++ FitAirlines_UI/Base/Components/BaseComboBox.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FitAirlines_UI
+{
+    public partial class BaseComboBox : ComboBox
+    {
+        
+        //
+        // MARK: - Constructors
+        //
+
+        public BaseComboBox()
+        {
+            InitializeComponent();
+        }
+
+        //
+        // MARK: - Protected methods
+        //
+
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+            Configure();
+        }
+
+        protected virtual void SetupStyling()
+        {
+            // TODO: JR
+        }
+
+        //
+        // MARK: - Private methods
+        //
+
+        void Configure()
+        {
+            BaseSetup();
+            SetupStyling();
+        }
+
+        void BaseSetup()
+        {
+            // TODO: JR
+        }
+    }
+}
