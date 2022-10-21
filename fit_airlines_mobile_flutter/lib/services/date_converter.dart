471ebe648501777cb6d2b7e9fc378b5f30bfd644
Josip Rezić <josip.rezic@hotmail.com>
2022-10-21 21:52:30 +0200

    Mobile: Update FlightDetailsView, add real data

--- fit_airlines_mobile_flutter/lib/services/date_converter.dart
+++ fit_airlines_mobile_flutter/lib/services/date_converter.dart
@@ -1,8 +1,25 @@
 import 'package:intl/intl.dart';
 import 'dart:core';
 
+class DateDecorator {
+  static String decorateMinDay(String? inputDate) {
+    if (inputDate == null) {
+      return '';
+    }
+    DateTime date = inputDate!.asDateTime();
+    var result = DateFormat('dd/MM/yyyy').format(date);
+    return result;
+  }
+}
+
 extension on DateTime {
   String asString() {
     return DateFormat('yyyy-MM-dd hh:mm:ss').format(this);
+  }
+}
+
+extension on String {
+  DateTime asDateTime() {
+    return DateTime.parse(this);
   }
 }
